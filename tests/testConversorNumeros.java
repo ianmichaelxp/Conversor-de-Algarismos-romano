f6040b69fdcbad13898f2a68a64ddf4d22c35bf6
Ian Michael <ianmichael.xp@gmail.com>
2020-05-18 17:05:12 -0300

    Atualização do import Assert

--- tests/testConversorNumeros.java
+++ tests/testConversorNumeros.java
@@ -1,7 +1,6 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,67 +19,67 @@ public class testConversorNumeros
 	@Test
 	public void testConverterI() 
 	{	
-		assertEquals(1, conversor.converterParaDecimal('I'));
+		Assert.assertEquals(1, conversor.converterParaDecimal('I'));
 	}
 	
 	@Test
 	public void testConverterV() 
 	{	
-		assertEquals(5, conversor.converterParaDecimal('V'));
+		Assert.assertEquals(5, conversor.converterParaDecimal('V'));
 	}
 	
 	@Test
 	public void testConverterX() 
 	{	
-		assertEquals(10, conversor.converterParaDecimal('X'));
+		Assert.assertEquals(10, conversor.converterParaDecimal('X'));
 	}
 	
 	@Test
 	public void testConverterL() 
 	{	
-		assertEquals(50, conversor.converterParaDecimal('L'));
+		Assert.assertEquals(50, conversor.converterParaDecimal('L'));
 	}
 	
 	@Test
 	public void testConverterC() 
 	{	
-		assertEquals(100, conversor.converterParaDecimal('C'));
+		Assert.assertEquals(100, conversor.converterParaDecimal('C'));
 	}
 	
 	@Test
 	public void testConverterD() 
 	{	
-		assertEquals(500, conversor.converterParaDecimal('D'));
+		Assert.assertEquals(500, conversor.converterParaDecimal('D'));
 	}
 	
 	@Test
 	public void testConverterM() 
 	{	
-		assertEquals(1000, conversor.converterParaDecimal('M'));
+		Assert.assertEquals(1000, conversor.converterParaDecimal('M'));
 	}
 	
 	@Test
 	public void testConverterII() 
 	{	
-		assertEquals(2, conversor.converterParaDecimal("II"));
+		Assert.assertEquals(2, conversor.converterParaDecimal("II"));
 	}
 	
 	@Test
 	public void testConverterIV() 
 	{	
-		assertEquals(4, conversor.converterParaDecimal("IV"));
+		Assert.assertEquals(4, conversor.converterParaDecimal("IV"));
 	}
 	
 	@Test
 	public void testConverterVI() 
 	{	
-		assertEquals(6, conversor.converterParaDecimal("VI"));
+		Assert.assertEquals(6, conversor.converterParaDecimal("VI"));
 	}
 	
 	@Test
 	public void testConverterXII() 
 	{	
-		assertEquals(12, conversor.converterParaDecimal("XII"));
+		Assert.assertEquals(12, conversor.converterParaDecimal("XII"));
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
